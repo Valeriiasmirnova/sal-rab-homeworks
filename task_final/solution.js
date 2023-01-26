0d0cb6b44405b4a7ca1bd58ea8b18346f0a931f1
Valeriis <powder666@mail.ru>
2023-01-26 20:54:01 +0300

    финал готов

--- task_final/solution.js
+++ task_final/solution.js
@@ -51,3 +51,21 @@ function sendRequest(name, phone, address, goods, sum) {
 
     return jsonData;
 }
+function sendRequest(name, phone, address, goods, sum) {
+    let data = {client: "", goods: [], order: {address, sum}};
+
+        data.client = `${name} ${phone}`;
+
+        let countOfGoods = goods.length;
+
+        for (let i = 0; i < countOfGoods; i += 1) {
+            data.goods.push({title: goods[i].title, count: goods[i].count});
+        }
+
+        data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
+
+        data.order.sum = sum;
+
+    let jsonData = JSON.stringify({data}); 
+    return jsonData;
+}
